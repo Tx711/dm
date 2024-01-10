4b365e9daa0b97cb7449930bbb6b0f3ea8a21582
Tarun Prasad <94122432+Tx711@users.noreply.github.com>
2024-01-10 12:41:02 +0530

    Update timestamp.js
    made changes to format

--- commands/timestamp.js
+++ commands/timestamp.js
@@ -1,14 +1,16 @@
-// timestamp.js
 
+// timestamp.js
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
- data: new SlashCommandBuilder()
-     .setName('timestamp')
-     .setDescription('Get the current timestamp'),
- async execute(interaction) {
-     const date = new Date();
-     const timestamp = date.toLocaleDateString('en-GB', { day: '2-digit', month: 'short', hour: '2-digit', minute: '2-digit' }) + ' ' + date.getFullYear();
-     await interaction.reply(timestamp);
- },
+  data: new SlashCommandBuilder()
+      .setName('time')
+      .setDescription('Get the current timestamp'),
+  async execute(interaction) {
+      const date = new Date();
+      const timestamp = date.toLocaleDateString('en-GB', { day: '2-digit', month: 'short', hour: '2-digit', minute: '2-digit' }) + ' ' + date.getFullYear();
+      await interaction.reply(timestamp);
+  },
 };
+
+
